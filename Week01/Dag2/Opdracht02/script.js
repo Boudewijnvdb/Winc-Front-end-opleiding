30be60689788e51821657f43b75af1c59ac0a719
Boudewijnvdb <62020353+Boudewijnvdb@users.noreply.github.com>
2020-05-05 13:52:34 +0200

    Add files via upload

---
+++ Week01/Dag2/Opdracht02/script.js
@@ -0,0 +1,19 @@
+console.log('Hallo Winc Academy Studenten');
+
+let x, y, z;
+
+
+
+x = 1230941 * 1823794;
+
+/*
+
+//Dit is een grote som
+console.log(x);
+
+y = 637263 / 54;
+
+console.log(y);
+
+*/
+
