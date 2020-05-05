0e0248f5012c37c68a763b87713056ad08ddc3c8
Boudewijnvdb <62020353+Boudewijnvdb@users.noreply.github.com>
2020-05-05 14:47:52 +0200

    Add files via upload

---
+++ Week01/Dag2/script.js
@@ -0,0 +1,28 @@
+
+const Myage = 19;
+
+if (Myage => 18) {
+
+    console.log('Welkom in de kroeg!!');
+
+} else {
+
+    console.log('je bent te jong');
+}
+
+const isFemale = false;
+
+if (isFemale === true) {
+    console.log('Welcome to ladies night!');
+} else {
+    console.log('Sorry not for you buddy');
+}
+
+const driverStatus = 'bob';
+
+if (driverStatus === 'bob') {
+    console.log('Drive safe');
+
+} else {
+    console.log('I will call a taxi for you')
+}
