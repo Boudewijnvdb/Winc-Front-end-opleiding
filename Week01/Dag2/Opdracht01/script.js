11a9efea74c591927ef1d53edeac004be551c307
Boudewijnvdb <62020353+Boudewijnvdb@users.noreply.github.com>
2020-05-05 10:18:04 +0200

    Add files via upload

---
+++ Week01/Dag2/Opdracht01/script.js
@@ -0,0 +1,41 @@
+console.log('hello Winc Academy');
+
+let name = "Boudewijn";
+
+
+console.log(name);
+
+let x, z, y;
+
+x = 5 + 5;
+
+z = x - 3;
+
+y = z * 10;
+
+console.log(x);
+
+console.log(z);
+
+console.log(y);
+
+
+let leeftijd;
+
+leeftijd = 27;
+
+console.log(typeof leeftijd);
+
+console.log(typeof "27");
+
+
+
+
+
+
+
+
+
+
+
+
