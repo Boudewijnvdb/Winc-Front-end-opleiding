20dccf8bec363245dd97de54e78d25644eb101fd
Boudewijnvdb <62020353+Boudewijnvdb@users.noreply.github.com>
2020-05-07 08:56:23 +0200

    Update script.js

--- Week01/Dag3/Test-eindopdracht:-Gues_the_number/script.js
+++ Week01/Dag3/Test-eindopdracht:-Gues_the_number/script.js
@@ -11,31 +11,25 @@ let guess = prompt('"Voer een nummer in van 0 tot 25 om te beginnen met raden" '
 
 
 
-let x = Math.floor(Math.random() * 25) + 1;
+let x = Math.floor(Math.random() * 10) + 1;
 
+let kansen = 0
 
-
-while (parseInt(guess) !== x) {
+while (parseInt(guess) !== x && kansen < 5) {
     alert('Dat is niet correct!');
-
-    guess = prompt('"Voer opnieuw een nummer in van 0 tot 25', []);
+    kansen++;
+    guess = prompt('"Voer opnieuuw een nummer in van 0 tot 25', []);
 
 
 }
 
-
-
-alert('Dat is correct!');
-
-
-
-alert('gefeliciteerd je hebt gewonnen');
-
-alert(`dag ${name}!`);
-
-
-
-
+if (parseInt(guess) === x) {
+    alert('Dat is correct!');
+    alert('gefeliciteerd je hebt gewonnen');
+    alert(`dag ${name}!`);
+} else {
+    alert('helaas teveel pogingen');
+}
 
 
 
