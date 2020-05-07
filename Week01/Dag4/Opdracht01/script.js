146eef3dbccc1708a31e459f26ea27b6f5cbd52f
Boudewijnvdb <62020353+Boudewijnvdb@users.noreply.github.com>
2020-05-07 11:01:14 +0200

    Add files via upload

---
+++ Week01/Dag4/Opdracht01/script.js
@@ -0,0 +1,13 @@
+/*
+document.getElementById("first-section")
+<div id=​"first-section">​…​</div>​<p>​Hallo ik ben een paragraaf​</p>​</div>
+
+document.getElementsByClassName('paragraph')
+HTMLCollection [div#second-section.paragraph, second-section: div#second-section.paragraph]
+let par = document.getElementsByClassName('paragraph')
+undefined
+console.log(par);
+VM457:1 HTMLCollection [div#second-section.paragraph, second-section: div#second-section.paragraph]
+undefined
+
+*/
