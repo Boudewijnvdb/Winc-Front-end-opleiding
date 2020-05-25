a4ae1cc9cc191ccd4af4c547526d4211873c929f
Boudewijn van den Berg <bjevandenberg@gmail.com>
2020-05-25 15:03:42 +0200

    opdrachten

---
+++ Week03/Dag5/ToDO/api-client.js
@@ -0,0 +1,47 @@
+// Get data
+const InputData = async () => {
+    const apiUrl = "https://wincacademydatabase.firebaseio.com/Boudewijn/Tasks.json"
+    try {
+        const res = await fetch(apiUrl, { method: "GET" });
+        const datas = await res.json();
+        return datas;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+//post data
+function postData() {
+    let task = document.getElementById('vul').value;
+
+    fetch("https://wincacademydatabase.firebaseio.com/Boudewijn/Tasks.json", {
+        method: 'POST',
+        body: JSON.stringify({ description: task, "done": true })
+    }).then((res) => res.json())
+        .then((data) => console.log(data))
+        .then((data) => console.log(data))
+        .catch((err) => console.log(err))
+
+
+}
+
+
+//Delete data
+const DeleteData = async (event) => {
+    const waarde = event.target.value;
+    const apiUrl = `https://wincacademydatabase.firebaseio.com/Boudewijn/Tasks/${waarde}.json`
+    try {
+        const res = await fetch(apiUrl, { method: "DELETE" });
+        const datas = await res.json();
+        return datas;
+
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
+
+
+
---
+++ Week03/Dag5/ToDO/script.js
@@ -0,0 +1,42 @@
+const subknop = document.getElementById('postData')
+const buttons = document.querySelectorAll("button");
+
+
+
+subknop.addEventListener('submit', postData);
+
+//transform data
+const doSomethingWithData = async function () {
+    const result = await InputData();
+    let tasks = Object.keys(result).map(key => ({
+        id: key,
+        description: result[key].description,
+        done: result[key].done
+    }));
+
+    console.log(tasks);
+
+    //put task in DOM
+    tasks.forEach(element => {
+        const item = document.createElement('li');
+        const takenLijst = document.getElementById('lijst');
+        item.innerHTML = element.description
+        takenLijst.appendChild(item);
+        const but = document.createElement('button');
+        but.innerHTML = "Delete";
+        item.appendChild(but);
+        but.value = element.id;
+        but.addEventListener('click', DeleteData);
+    })
+}
+
+
+
+
+
+
+doSomethingWithData();
+
+
+
+
